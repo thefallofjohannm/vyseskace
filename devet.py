d905849cb30b4fe867d4a921978ce2c94b9a8a66
Tibor Malinský <tibormalinsky@Tibor-MacBook-Air.local>
2023-05-24 11:57:00 +0200

    add current progress

---
+++ devet.py
@@ -0,0 +1,84 @@
+"""
+ Pygame base template for opening a window
+
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+
+import pygame
+
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+
+pygame.init()
+
+def draw_tree(screen, x, y):
+    pygame.draw.rect(screen, BROWN, [60+x, 170+y, 30, 45])
+    pygame.draw.polygon(screen, GREEN, [[150+x,170+y],[75+x,20+y], [x,170+y]])
+    pygame.draw.polygon(screen, GREEN, [[140+x,120+y], [75+x,y], [10+x,120+y]])
+
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("My Game")
+
+# Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+rect_x = 50
+rect_y = 50
+
+rect_x_change = 5
+rect_y_change = 5
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+vscht
+    # --- Game logic should go here
+
+    # --- Screen-clearing code goes here
+
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(BLACK)
+    draw_tree(screen, 0, 300)
+    # --- Drawing code should go here
+    pygame.draw.rect(screen, WHITE, [rect_x, 50, 50, 50])
+    rect_x += rect_x_change
+    rect_y += rect_y_change
+
+    # Bounce the rectangle if needed
+    if rect_y > 450 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+    if rect_x > 650 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+
+    for i in range(50):
+        x = random.randrange(0, 400)
+        y = random.randrange(0, 400)
+        pygame.draw.circle(screen, WHITE, [x, y], 2)
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+
+# Close the window and quit.
+pygame.quit()
