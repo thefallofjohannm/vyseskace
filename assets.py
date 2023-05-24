bdc7a83a12d1ea2661760d917e2da78634a83ddd
Tibor Malinský <tibormalinsky@Tibor-MacBook-Air.local>
2023-05-24 15:36:40 +0200

    background change

--- assets.py
+++ assets.py
@@ -10,6 +10,7 @@ GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BROWN = (200, 200, 100)
 BLUE = (0, 0, 255)
+LBLUE = (200, 200, 255)
 
 
 def draw_tree(screen, x, y):
@@ -17,38 +18,40 @@ def draw_tree(screen, x, y):
     pygame.draw.polygon(screen, GREEN, [[150 + x, 170 + y], [75 + x, 20 + y], [x, 170 + y]])
     pygame.draw.polygon(screen, GREEN, [[140 + x, 120 + y], [75 + x, y], [10 + x, 120 + y]])
 
+    # toto je strom, který se tam vyobrazí do pozadí a nic nedělá. Dá se to předělat a nakreslit něco jiného
 
 def get_background_image(w, h):
     img = Image.open("background.jpeg")
     img.resize((w, h))
     return pygame.image.frombuffer(img.tobytes(), (w, h), "RGB")
 
+    #toto by mělo nahrát obrázek, ale zaítm to nefunguje, tak na to kašlu
 
 def draw_background(screen):
-    screen.fill(BLACK)
+    screen.fill(LBLUE)
     draw_tree(screen, 0, 450)
-
+    #toto je moje pozadí, což je tedka cerna plocha se stromem
 
 def draw_ground(screen):
     pygame.draw.rect(screen, WHITE,
                      [0, screen.get_height() - 35, screen.get_width(), 35],
                      30)
-
+    # toto je spodní hranice. Je tam spíš z graifckých než užitových důvodů
 
 def snow(screen):
-    for i in range(50):
-        x = random.randrange(0, screen.get_width())  # kdyz zmenim ikni bzude to fungoat
+    for i in range(1):
+        x = random.randrange(0, screen.get_width())  # kdyz zmenim okno bude to fungoat
         y = random.randrange(0, screen.get_height())
-        pygame.draw.circle(screen, WHITE, [x, y], 2)
+        pygame.draw.circle(screen, WHITE, [x, y], 4)
+    #toto je sníh, ale funguje tak, že se mi to někde ranodm ukazuje, to je naprd, takže ot pak předelám
 
-
-class Block:
+class Block: #toto jsou moje bloky, na které chci skákat
     def __init__(self, screen, y):
         self.screen = screen
         self.width = 50
         self.height = 10
         self.color = BLUE
-        self.position_x = random.randint(0, screen.get_width() - self.width)
+        self.position_x = random.randint(self.width, screen.get_width() - self.width)
         self.position_y = y
 
     def draw(self):
@@ -90,3 +93,4 @@ class Player:
         pygame.draw.rect(self.screen,
                          self.color,
                          [self.position[0], self.position[1], self.size, self.size], self.size)
+
